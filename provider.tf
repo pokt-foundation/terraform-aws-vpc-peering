11be9ac058bdace1ee8902f59799f069e0ddec8b
TakGN <79975025+TakGN@users.noreply.github.com>
2022-08-10 01:24:10 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 3.37"
+      version               = "3.74.3"
       configuration_aliases = [aws.this, aws.peer]
     }
   }
