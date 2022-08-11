a3652c4f54298d1b01b05ee75bf6386c2a2e1eed
TakGN <79975025+TakGN@users.noreply.github.com>
2022-08-11 12:10:40 +0200

    ignore update changes on destination_cidr_block

--- main.tf
+++ main.tf
@@ -60,6 +60,12 @@ resource "aws_route" "this_routes" {
   route_table_id            = local.this_routes[count.index].rts_id
   destination_cidr_block    = local.this_routes[count.index].dest_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
+  
+  	lifecycle {
+		ignore_changes = [
+      destination_cidr_block
+		]
+    }
 }
 
 ###################
